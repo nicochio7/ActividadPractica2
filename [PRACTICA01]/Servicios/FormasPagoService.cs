12370eb3c9a8175dfe1edd28e1af065cabb5aa7b
Nico Chiofalo <nicolas.mauricio.chiofalo@gmail.com>
2024-09-17 21:18:24 -0300

    first commit

---
+++ [PRACTICA01]/Servicios/FormasPagoService.cs
@@ -0,0 +1,45 @@
+﻿using _PRACTICA01_.Datos.Implementaciones;
+using _PRACTICA01_.Datos.Interfaces;
+
+using Practica01.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica01.Servicios
+{
+    public class FormasPagoService
+    {
+        IFormasPagoRepository _repository;
+        public FormasPagoService()
+        {
+            _repository = new FormasPagoRepositoryADO();
+
+        }
+
+        public List<FormasPago> GetFormasPagos()
+        {
+
+        return _repository.GetAll(); 
+        }
+        
+        public bool Save(FormasPago fp)
+        {
+            return _repository.Save(fp);
+        }
+
+        public FormasPago getByID(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public bool delete(int id)
+        {
+            return _repository.Delete(id);
+        }
+
+    }
+}
+
